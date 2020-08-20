ff2560b1e792fe9e3c939faa9603362098c659a4
ITMASTER\m.fravezzi <m.fravezzi@almaviva.it>
2020-08-20 15:12:46 +0200

    e2e Test

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -2,22 +2,39 @@ import { AppPage } from './app.po';
 import { browser, logging } from 'protractor';
 
 describe('workspace-project App', () => {
-  let page: AppPage;
+	let page: AppPage;
 
-  beforeEach(() => {
-    page = new AppPage();
-  });
+	beforeEach(() => {
+		page = new AppPage();
+	});
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to angular-test!');
-  });
+	it('should display welcome message', () => {
+		page.navigateTo();
+		expect(page.getTitleText()).toEqual('La mia calcolatrice');
+	});
 
-  afterEach(async () => {
-    // Assert that there are no errors emitted from the browser
-    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
-    expect(logs).not.toContain(jasmine.objectContaining({
-      level: logging.Level.SEVERE,
-    } as logging.Entry));
-  });
+	it('should display result value', () => {
+		page.navigateTo();
+		page.getNum1Input().clear()
+			.then(() =>
+				page.getNum1Input().sendKeys('52')
+			)
+		page.getNum2Input().clear()
+			.then(() =>
+				page.getNum2Input().sendKeys('3')
+			)
+
+		page.getOperatorOptionsProduct().click();
+		page.getCalculateButton().click();
+
+		expect(page.getResultValue()).toEqual('156');
+	});
+
+	afterEach(async () => {
+		// Assert that there are no errors emitted from the browser
+		const logs = await browser.manage().logs().get(logging.Type.BROWSER);
+		expect(logs).not.toContain(jasmine.objectContaining({
+			level: logging.Level.SEVERE,
+		} as logging.Entry));
+	});
 });
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,35 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element, ElementFinder } from 'protractor';
 
 export class AppPage {
-  navigateTo() {
-    return browser.get(browser.baseUrl) as Promise<any>;
-  }
+	navigateTo() {
+		return browser.get(browser.baseUrl) as Promise<any>;
+	}
 
-  getTitleText() {
-    return element(by.css('app-root h1')).getText() as Promise<string>;
-  }
+	getTitleText() {
+		return element(by.css('app-root h1')).getText() as Promise<string>;
+	}
+
+	getNum1Input(): ElementFinder {
+		return element(by.id("num1"));
+	}
+
+	getNum2Input(): ElementFinder {
+		return element(by.id('num2'));
+	}
+
+	getOperatorSelect(): ElementFinder {
+		return element(by.id('operator'));
+	}
+
+	getOperatorOptionsProduct(): ElementFinder {
+		return this.getOperatorSelect().element(by.cssContainingText('option', '*'));
+	}
+
+	getCalculateButton(): ElementFinder {
+		return element(by.id('calculate'));
+	}
+
+	getResultValue(): Promise<string> {
+		return element(by.id('result')).getAttribute('value') as Promise<string>;
+	}
 }
